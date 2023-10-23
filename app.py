c51690f33f4ea77b7e4ce0cacc75001933c6ffcf
padua-unfei <146841527+padua-unfei@users.noreply.github.com>
2023-10-23 14:08:32 +0000

    atualizado

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd 
 
+st.set_page_config(
+    'Pedro - ECOP06',
+    'https://unifei.edu.br/wp-content/themes/twentytwelve-child/img/cabecalho/logo-unifei-oficial.png')
+
+st.tittle('Pagina Teste de ECOP06')
+
+esportes = pd.read_csv(
+    'https://github.com/MainakRepositor/Datasets/raw/master/GeneralEsportData.csv',
+    encoding ='latin-1')
+
+st.dataframe(esportes)
